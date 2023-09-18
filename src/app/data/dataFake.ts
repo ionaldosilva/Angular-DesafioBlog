1bf06ad441b2a0a0fbeda3379c3143b2cebbf0a0
Ionaldo Silva <ionaldo_araujo@hotmail.com>
2023-09-18 19:33:25 -0300

    create content page

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+    {
+        "id":1,
+        "photo":"https://www.cnnbrasil.com.br/wp-content/uploads/sites/12/2022/08/Phantom_Galaxy_across_the_spectrum_article.jpg?w=876&h=484&crop=1",
+        "title":"NEBULOSA de CARINA",
+        "description":"Imagem da Nebulosa de Carina, tirada pelo telescópio James Webb da NASA",
+    },
+    { 
+        "id":2,
+        "photo":"https://s2.glbimg.com/1jj-J5vz7H74C4SkN9iHJzA_Xm0=/e.glbimg.com/og/ed/f/original/2020/06/21/stsci-h-p2031b-m-2000x1778.png",
+        "title":"NEBULOSA BORBOLETA",
+        "description":"Imagem da Nebulosa da Borboleta, tirada pelo telescópio Hubble da NASA",
+    },
+    { 
+        "id":3,
+        "photo":"https://s2-valor.glbimg.com/TH1zmXIAdSTB0GUPKvA520kGyb4=/0x0:2019x2048/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_63b422c2caee4269b8b34177e8876b93/internal_photos/bs/2023/N/j/BZB5C3SoqNsNmLaFLBBw/nebulosa-do-anel-capturada-pelo-james-webb.jpg",
+        "title":"NEBULOSA DO ANEL",
+        "description":"Imagem da Nebulosa do Anel, tirada pelo telescópio James Webb da NASA",
+    },
+    { 
+        "id":4,
+        "photo":"https://classic.exame.com/wp-content/uploads/2022/09/nebulosa-tarantula-imagem-james-webb.jpg?quality=70&strip=info&w=1024",
+        "title":"NEBULOSA DA TARANTULA",
+        "description":"Imagem da Nebulosa da Tarantula, localizada a 161mil anos-luz da terra, tirada pelo telescópio James Webb da NASA",
+    }
+]
